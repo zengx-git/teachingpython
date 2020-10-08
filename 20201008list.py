9a453ee20536e2eb2715497056c46249ba3ee148
zengx-git <62016468+zengx-git@users.noreply.github.com>
2020-10-08 09:24:02 +0800

    Add files via upload

---
+++ 20201008list.py
@@ -0,0 +1,15 @@
+#问题引入,要求把输入的所有信息记录下来，供后面其它地方使用
+import os
+import time
+
+while True:
+    strInput = input("请输入内容：")
+    print(strInput)
+    if strInput.upper() == "Q":
+        print("输入结束")
+        break;
+
+#怎么把输入的内容找出来呢？？？
+print("????")
+# print(listStore)
+time.sleep(100)
